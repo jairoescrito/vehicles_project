3bdeb31dd6fe38731d6030335bc03c3d8cb21b11
jairoescrito <jairoescrito@gmail.com>
2025-11-24 13:13:46 -0500

    Inicio en la creación de la apicación, proyecto Sprint 7

---
+++ app.py
@@ -0,0 +1,14 @@
+
+# Importar librerías
+import pandas as pd
+import plotly.graph_objects as go
+import streamlit as st
+
+# Leer el conjunto de datos desde el archivo plano
+data = pd.read_csv('vehicles_us.csv')
+
+# Crear título de la aplicación
+st.header('Oferta de vehículos')
+
+# Contruir la aplicación
+st.write('En construcción')
